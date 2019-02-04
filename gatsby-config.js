e52d181f183ecd95399a0c518731e91be13bf083
Easton Gorishek <ieaston26@gmail.com>
2019-02-04 14:16:06 -0800

    trying to deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,7 @@
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        implementation: require('sass'),
-      },
-    },
+    `gatsby-plugin-sass`,
+
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
