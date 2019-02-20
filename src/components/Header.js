4e120b96a26959878689ac8502f0b5f4971fd32d
Easton Gorishek <ieaston26@gmail.com>
2019-02-19 20:39:03 -0800

    blah

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,16 +42,10 @@ class Header extends PureComponent {
     if(menu) {
       body.style.overflowY = 'hidden';
       body.style.position = 'relative';
-      document.ontouchmove = e => {
-        e.preventDefault();
-      };
     }
     else {
       body.style.overflowY = 'visible';
       body.style.position = 'initial';
-      document.ontouchmove = e => {
-        return true;
-      };
     }
   }
 
