2972aea4337f4611dbd42355f76872b0d6026e00
Easton Gorishek <ieaston26@gmail.com>
2019-03-13 21:16:21 -0700

    fixed project coloring

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -3,6 +3,12 @@ import styles from './about.module.css';
 
 export default props => (
   <div className={styles.about} id={props.id}>
-    <p>Software Engineer with a passion to solve challenging problems and automate tasks through innovative and seamless apps. My past work and travel experiences have cultivated my ability to adapt quickly, navigate through unknown territories, communicate, empathize, and collaborate within a team environment. In short, I'm a lifelong learner, with a keen interest in tech, that strives to develop apps that leave a lasting impression for the end user. </p>
+    <h1>Hi, I'm Easton. I'm a...</h1>
+    <ul>
+      <li>Coder</li>
+      <li>Traveler</li>
+      <li>Coffee addict</li>
+    </ul>
+    <p>Welcome to my page.</p>
   </div>
 );
