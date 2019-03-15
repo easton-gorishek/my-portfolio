79edf7f378038b1ddcd7d0b0dd274e2f1b36736c
Easton Gorishek <ieaston26@gmail.com>
2019-03-15 09:40:31 -0700

    edited about section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -4,18 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default props => (
   <div className={styles.about} id={props.id}>
-    <section className={styles.intro}>
-      <p>Hi, I'm Easton. I'm a...</p>
-      <ul>
-        <li><FontAwesomeIcon icon='code' /> Software Engineer</li>
-        <li><FontAwesomeIcon icon='globe-americas' /> Traveler</li>
-        <li><FontAwesomeIcon icon='coffee' /> Coffee addict</li>
-      </ul>
-    </section>
-    <section className={styles.welcome}>
-      <p className={styles.welcomeText}>Welcome to my page.</p>
-      {/* <p className={styles.peek}>Take a peek.</p> */}
-      <p className={styles.downArrow}><FontAwesomeIcon icon='arrow-down' /></p>
-    </section>
+    <ul>
+      <li id={styles.softwareEngineer}><FontAwesomeIcon icon='code' /> Software Engineer</li>
+      <li id={styles.traveler}><FontAwesomeIcon icon='globe-americas' /> Traveler</li>
+      <li id={styles.coffee}><FontAwesomeIcon icon='coffee' /> Coffee Addict</li>
+    </ul>
   </div>
 );
