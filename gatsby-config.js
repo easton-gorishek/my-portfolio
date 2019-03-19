e497507fb774d4754f95d113ee7a802919a0c284
Easton Gorishek <ieaston26@gmail.com>
2019-03-18 17:15:33 -0700

    added .env files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${ process.env.NODE_ENV }`,
+});
+
 module.exports = {
   plugins: [
     {
@@ -18,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `q5m4ayfot88b`,
-        accessToken: `8ed59997235f069707b49a4aa2f9401ec8a97afb55da957a1280ec308e3534b5`
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     }
   ]
