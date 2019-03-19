b131e0f8e2fcd02426b5ce57fb584cf77153b8e1
Easton Gorishek <ieaston26@gmail.com>
2019-03-19 16:42:12 -0700

    changed contact design for desktop

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -5,20 +5,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default props => (
   <div className={styles.contact} id={props.id}>
     <p>LET'S CHAT <FontAwesomeIcon icon='coffee' /></p>
-    <a href='mailto:ieaston26@gmail.com' title='email'>
-      <FontAwesomeIcon className={styles.icons} icon='envelope' />
-    </a>
-    <a
-      href='https://www.linkedin.com/in/easton-gorishek/'
-      target='_blank'
-      title='LinkedIn'>
-      <FontAwesomeIcon className={styles.icons} icon={['fab', 'linkedin']} />
-    </a>
-    <a
-      href='https://github.com/easton-gorishek'
-      target='_blank'
-      title='GitHub'>
-      <FontAwesomeIcon className={styles.icons} icon={['fab', 'github']} />
-    </a>
+    <section>
+      <a href='mailto:ieaston26@gmail.com' title='email'>
+        <FontAwesomeIcon className={styles.icons} icon='envelope' />
+      </a>
+      <a
+        href='https://www.linkedin.com/in/easton-gorishek/'
+        target='_blank'
+        title='LinkedIn'>
+        <FontAwesomeIcon className={styles.icons} icon={['fab', 'linkedin']} />
+      </a>
+      <a
+        href='https://github.com/easton-gorishek'
+        target='_blank'
+        title='GitHub'>
+        <FontAwesomeIcon className={styles.icons} icon={['fab', 'github']} />
+      </a>
+    </section>
   </div>
 );
