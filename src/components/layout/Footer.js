f40a9dc9334193a0fbf901abea27c1a967690c61
Easton Gorishek <ieaston26@gmail.com>
2019-09-19 20:33:33 -0700

    removed text from footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,6 +4,5 @@ import styles from './footer.module.css';
 export default () => (
   <footer className={styles.footer}>
     <p className={styles.copyRight}>&copy; Easton Gorishek 2019</p>
-    <p>testing 123</p>
   </footer>
 );
